86fc02411075043c275145dc820f19a5ff93774a
BarzaGuignol <73914713+BarzaGuignol@users.noreply.github.com>
2022-11-28 15:52:37 +0100

    Update AreYou.jsx

--- src/components/AreYou.jsx
+++ src/components/AreYou.jsx
@@ -17,7 +17,7 @@ export default function AreYou({ setPath }) {
             <button className='buttonAreYou' onClick={() => setPath('/')}>Yes</button>
             <button className='buttonAreYou' onClick={handleChoice}>No</button>
             {choice ? <div>
-                <audio autoPlay src="/src/assets/sound/screamScarry.mp3"></audio>
+                <audio autoPlay src="assets/screamScarry.1bbb4a1f.mp3"></audio>
             </div>: ""}
         </div>
     </div>
